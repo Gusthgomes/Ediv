d861db0b0b4c98cae24500bc35cb425f173ea6ae
Gustavo Gomes <heiqueenn@gmail.com>
2023-06-14 13:15:40 -0300

    modified forms and models

--- apps/authenticacao/forms.py
+++ apps/authenticacao/forms.py
@@ -5,20 +5,21 @@ from .models import Users
 class UserChangeForm(forms.UserChangeForm):
     class Meta(forms.UserChangeForm.Meta):
         model = Users
-        
+
 class UserCreationForm(forms.UserCreationForm):
     class Meta(forms.UserCreationForm.Meta):
         model = Users
-        
-class RegisterForm(UserChangeForm):
-    '''Formulário para criação de usuários sem permissões administrativas a partir do email, first name e senha'''
-    class Meta(UserChangeForm.Meta):
+
+
+class RegisterForm(UserCreationForm):
+    '''Formulário para cadastro de usuários sem permissões administrativas apartir do Email, first_name e senha'''
+
+    class Meta(UserCreationForm.Meta):
         fields = ('first_name', 'email')
-        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             if 'password' in field:
                 self.fields[field].help_text = None
             self.fields[field].widget.attrs.update({'class': 'form-control'})
-        
--- apps/authenticacao/models.py
+++ apps/authenticacao/models.py
@@ -7,33 +7,31 @@ class UserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
         return user
-    
+
     def create_user(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
-    
+
     def create_superuser(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Super User should be set to True")
-        
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Super User should be set to True")
-        
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Super usuário deve ter is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Super usuário deve ter is_superuser=True')
+
         return self._create_user(email, password, **extra_fields)
-        
 
 class Users(AbstractUser):
     username = models.CharField(max_length=150, null=True, blank=True)
     email = models.EmailField(unique=True)
-    EMAIL_FIELD = 'email'
+    EMAIL_FIELD = "email"
     USERNAME_FIELD = 'username'
-    Objects = UserManager()
-    
-    def __str__(self):
+    objects = UserManager()
+
+    def __str__(self) -> str:
         return self.email
